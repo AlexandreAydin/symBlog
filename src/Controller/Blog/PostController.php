fddda14821fbd71a5720cbec0ed86334d2659b4e
AlexandreAydin <alexandreaydin1993@gmail.com>
2023-05-15 23:01:39 +0200

    ✨ find the post

---
+++ src/Controller/Blog/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Blog;
+
+use App\Repository\Post\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'app_post')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('pages/post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+}
